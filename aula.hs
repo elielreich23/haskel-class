1e6b8cbf5778d7550dde0e46b720ec437dc85452
ELIEL DOUNA <97183542+elielreich23@users.noreply.github.com>
2023-06-27 16:12:54 -0300

    Update aula.hs

--- aula.hs
+++ aula.hs
@@ -247,6 +247,17 @@ type Agenda = [Contato]
 procurar nome [] = "nao encontrado"
 procurar nome ((Pessoa n t d):xs) = if n == nome then t else procurar nome xs
 
+--Aula 20: Árvores
+data Arvore a = No a (Arvore a)(Arvore a)|Folha deriving Show
+
+ins e Folha = No e Folha Folha
+ins e (No x esq dir)|e==x = No x esq dir
+                    |e < x = No x (ins e esq) dir
+                    |otherwise = No x esq (ins e dir)
+
+insListaArvore::[a] -> Arvore a
+insListaArvore [] = Folha
+insListaArvore (x:xs) = ins x (insListaArvore xs)
 
 --exo aula 01/07
 numLines (x:xs) = aux 1 (x:xs)
