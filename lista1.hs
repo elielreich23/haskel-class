36b131a298e787a4a24be9d056d8f5a930443b8e
ELIEL DOUNA <97183542+elielreich23@users.noreply.github.com>
2023-07-13 14:03:32 -0300

    Update lista1.hs

--- lista1.hs
+++ lista1.hs
@@ -23,3 +23,36 @@ somaPot2m m n = 2^n * m  +somaPot2m m (n - 1)
 ehdiv n 1 = True
 ehdiv n m = if rem n m == 0 then False else ehdiv n (m-1)
 primo n = ehdiv n(n-1)
+
+--ADICIONAL CODE 
+    --exo 1
+isTriangulo a b c = if a + b>c && b+c>a && c+a>b then True else False
+
+--exo 2
+typeTriangle x y z = if x == y && y == z then "equilatero"
+else if x == y || y == z || x == z then "isoceles" else "escaleno" 
+
+--exo3
+triangle a b c = if isTriangulo a b c then if a == b && b == c  then "equilatero"
+else if a == b || b == c || c == a then "isoceles" else "escaleno"
+else "not a triangle"
+
+--exo4
+prime a = if rem a 2 == 0 then a else a-1
+sumEven 0 = 0
+sumEven a = prime a + sumEven (prime a - 2)
+
+--exo5
+somaPot2m m 0 = m
+somaPot2m m n = 2^n * m +somaPot2m m (n-1)
+
+--exo6
+ehdiv n 1 = True
+ehdiv n m = if rem n m == 0 then False else ehdiv n (m-1)
+primo n = ehdiv n(n-1)
+
+pares [] = 0
+pares (x:xs) = if rem x 2 == 0 then x else if x:pares xs
+
+occorence _ [] = 0
+occorence n(x:xs) = if n == x then 1 +occorence n xs else occorence n xs
